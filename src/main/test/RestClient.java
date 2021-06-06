82d8d0999837da5e6124dacf99ec27f702279623
Vladus7 <delson300013@gmail.com>
2021-06-06 21:07:25 +0300

    Access-Control-Allow-Origin filter

--- src/main/test/RestClient.java
+++ src/main/test/RestClient.java
@@ -1,10 +1,12 @@
 import com.model.User;
 import com.model.UserDto;
+import com.model.UserRowDto;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.List;
 
 public class RestClient {
 
@@ -20,4 +22,11 @@ public class RestClient {
                 .request(MediaType.APPLICATION_JSON)
                 .get(UserDto.class);
     }
+
+//    public List<UserRowDto> getAllUsers(){
+//        client.target(REST_URI)
+//                .path(String.valueOf(id))
+//                .request(MediaType.APPLICATION_JSON)
+//                .get(UserDto.class);
+//    }
 }
